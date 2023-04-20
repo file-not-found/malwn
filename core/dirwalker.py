86b6c1353532fa075653b3004489cadff43d848f
file-not-found <file-not-found@tritonmail.de>
2023-04-20 15:18:46 +0200

    dynamic rel-/abspath output

--- core/dirwalker.py
+++ core/dirwalker.py
@@ -4,22 +4,26 @@ import os
 
 def get_files(paths, extensions=["",], recursive=False):
     for path in paths:
+        if path.startswith('/'):
+            to_path = os.path.abspath
+        else:
+            to_path = os.path.relpath
         if os.path.isdir(path):
             if recursive:
                 for r, d, f in os.walk(path):
                     for filename in f:
                         for extension in extensions:
                             if filename.endswith(extension):
-                                yield os.path.relpath(os.path.join(r, filename))
+                                yield to_path(os.path.join(r, filename))
             else:
                 for filename in os.listdir(path):
                     for extension in extensions:
                         if filename.endswith(extension):
-                            yield os.path.relpath(os.path.join(path, filename))
+                            yield to_path(os.path.join(path, filename))
         elif os.path.isfile(path):
             for extension in extensions:
                 if path.endswith(extension):
-                    yield os.path.relpath(path)
+                    yield to_path(path)
 
 def get_all_files(args):
     return get_files(args.file, recursive=args.recursive)
